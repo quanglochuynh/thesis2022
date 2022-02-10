747a3aade002375d820b26ef0cb6ce9f51cd48c0
Loc Huynh <quanglochuynh@gmail.com>
2022-02-10 23:57:52 +0700

    finishing NN framework

--- Python/neural_network.py
+++ Python/neural_network.py
@@ -38,7 +38,7 @@ class MultilayerNeuralNetwork:
             for i in range(len(self.weight_matrix)):
                 data_matrix = Matrix.multiply(self.weight_matrix[i], data_matrix)
                 data_matrix = Matrix.add(data_matrix, self.bias_matrix[i])
-                data_matrix.data = map(sigmoid, data_matrix.data)
+                data_matrix = Matrix.map(data_matrix, sigmoid)
             return Matrix.matrix_2_array(data_matrix)
     
     def train(self, input_array, target_array):
@@ -49,15 +49,11 @@ class MultilayerNeuralNetwork:
             print("Wrong target dimension!")
             return -2
         else:
-            # print(input_array)
-            # print(target_array)
             #feed-forward
             layer_result_matrix_array = []
             feed_matrix = Matrix.array_2_matrix(input_array)
             layer_result_matrix_array.append(feed_matrix)
             for i in range(len(self.weight_matrix)):
-                # print(feed_matrix.data)
-                # print(self.weight_matrix[i].data)
                 feed_matrix = Matrix.multiply(self.weight_matrix[i], feed_matrix)
                 feed_matrix = Matrix.add(feed_matrix, self.bias_matrix[i])
                 feed_matrix = Matrix.map(feed_matrix, sigmoid)
@@ -66,32 +62,17 @@ class MultilayerNeuralNetwork:
             #BACK-propagation
             target_matrix = Matrix.array_2_matrix(target_array)
             error_matrix = Matrix.subtract(target_matrix, feed_result_matrix)
-            # print(target_matrix.data)
-            # print(feed_matrix.data)
-            # print(error_matrix.data)
             for i in range(len(self.layer_array)-2, -1, -1):
-                # print(i)
-                # print(error_matrix.data)
                 gradient_matrix = Matrix.map(layer_result_matrix_array[i+1], dsigmoid)
-                # print(gradient_matrix.data)
+                gradient_matrix = Matrix.hadamard(gradient_matrix, error_matrix)
                 gradient_matrix.data = np.multiply(gradient_matrix.data, self.learning_rate)
                 delta = Matrix.multiply(gradient_matrix, Matrix.transpose(layer_result_matrix_array[i]))
-                # print(self.weight_matrix[i].data)
-                # print(delta.data)
                 self.weight_matrix[i] = Matrix.add(self.weight_matrix[i], delta)
-                # print(self.weight_matrix[i].data)
                 self.bias_matrix[i] = Matrix.add(self.bias_matrix[i], gradient_matrix)
                 if (i==-1): continue
                 weight_transposed = Matrix.transpose(self.weight_matrix[i])
                 error_matrix = Matrix.multiply(weight_transposed, error_matrix)
             return 0
-
-
-
-
-
-
-
 
 
 class Epoch:
@@ -100,38 +81,25 @@ class Epoch:
         self.target_array = target_array
 
 
-# nn = MultilayerNeuralNetwork([5,4,3,2], 0.1)
-# # res = nn.feed_forward([1,2,3,4,5])
-# nn.train(np.transpose([1,2,3,4,5]), [2, 3])
+dataset = []
+dataset.append(Epoch([1,0], [1]))
+dataset.append(Epoch([0,1], [1]))
+dataset.append(Epoch([0,0], [0]))
+dataset.append(Epoch([1,1], [0]))
 
 # dataset = []
-# dataset.append(Epoch([1,0], [1]))
-# dataset.append(Epoch([0,1], [1]))
-# dataset.append(Epoch([0,0], [0]))
-# dataset.append(Epoch([1,1], [0]))
-
-dataset = []
-dataset.append(Epoch([1,0,0], [0,0,1]))
-dataset.append(Epoch([0,1,0], [1,0,0]))
-dataset.append(Epoch([0,0,1], [0,1,0]))
+# dataset.append(Epoch([1,0,0], [0,0,1]))
+# dataset.append(Epoch([0,1,0], [1,0,0]))
+# dataset.append(Epoch([0,0,1], [0,1,0]))
 
 
-mlnn = MultilayerNeuralNetwork([3, 3, 3], 1)
+mlnn = MultilayerNeuralNetwork([2, 2, 1], 0.5)
 
-print(mlnn.feed_forward(dataset[0].input_array))
-print(mlnn.feed_forward(dataset[1].input_array))
-print(mlnn.feed_forward(dataset[2].input_array))
-# print(mlnn.feed_forward(dataset[3].input_array))
-
-for i in range(100  ):
+for i in range(8000):
     data = rd.choice(dataset)
-    # print(data.target_array)
-    # print(data.input_array)
-    print(data.target_array)
     k = mlnn.train(data.input_array, data.target_array)
-    # print(k)
 
 print(mlnn.feed_forward(dataset[0].input_array))
 print(mlnn.feed_forward(dataset[1].input_array))
 print(mlnn.feed_forward(dataset[2].input_array))
-# print(mlnn.feed_forward(dataset[3].input_array))
+print(mlnn.feed_forward(dataset[3].input_array))
