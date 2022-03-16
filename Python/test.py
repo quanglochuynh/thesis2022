09366325b62a2e4bc2d87c491076e1ea45a3e7ac
Flint_Huynh <quanglochuynh@gmail.com>
2022-03-16 14:12:01 +0700

    .

--- Python/test.py
+++ Python/test.py
@@ -1,23 +1,27 @@
 import cv2
 import numpy as np
 
-di = '/Users/lochuynhquang/Documents/thesis2022/Python/data/bean2.JPG'
-
+# di = '/Users/lochuynhquang/Documents/thesis2022/Python/data/bean2.JPG'
+di ='/home/flint/Documents/thesis2022/Python/data/bean2.JPG'
 # image = cv2.imread(di + 'image (1).JPG')
 image = cv2.imread(di)
 image = cv2.resize(image, (256,256))
 kernel1 = np.array([[-1, -1, -1],
                     [-1,  8, -1],
                     [-1, -1, -1]])
-edge_detect = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
 
-# cv2.imshow('Identity', edge_detect)
-cv2.imwrite('/Users/lochuynhquang/Documents/thesis2022/Python/data/bean2_ED.JPG' ,edge_detect)
+kernel2 = np.array([[-1/2, 1, -1/2],
+                    [-1/2, 1, -1/2],
+                    [-1/2, 1, -1/2]])
+edge_detect = cv2.filter2D(src=image, ddepth=-1, kernel=2)
+
+cv2.imshow('Identity', edge_detect)
+# cv2.imwrite('/Users/lochuynhquang/Documents/thesis2022/Python/data/bean2_ED.JPG' ,edge_detect)
 
 # print(type(image))
 # print(np.shape(image))
 
-# cv2.waitKey(0) 
-# cv2.destroyAllWindows()
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
 
 
