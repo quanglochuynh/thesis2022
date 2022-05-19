11f293ce466862d17bcae6b466e0fa98ae9623b0
Flint Huynh <quanglochuynh@gmail.com>
2022-05-19 14:01:22 +0700

    .

--- The_new_thesis_approach/image_extractor.py
+++ The_new_thesis_approach/image_extractor.py
@@ -2,7 +2,8 @@ from statistics import median
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt 
-from scipy.stats import skew, kurtosis, mode, median_absolute_deviation
+from scipy.stats import skew, kurtosis
+import statistics as st
 
 # gm = 0.75
 # dx = 10
@@ -36,7 +37,7 @@ def hsv_filter(im):
     sp = np.shape(image)
     for i in range(sp[0]):
         for j in range(sp[1]):
-            if image[i][j][2]<20:
+            if image[i][j][2]<25:
                 image[i][j] = np.array([0, 0, 0])
     return image 
 
@@ -67,10 +68,7 @@ def statistic_extractor(image):
     total_pix = shape[0]*shape[1]
     (sumR, sumG, sumB) = (np.sum(im[:, :, 0]), np.sum(im[:, :, 1]), np.sum(im[:, :, 2]))
     meanR, meanG, meanB = sumR/total_pix, sumG/total_pix, sumB/total_pix
-
-    modeR, modeG, modeB = mode(im[:, :, 0].flatten(), axis=None), mode(im[:, :, 1].flatten(), axis=None), mode(im[:, :, 2].flatten(), axis=None)
-    medianR, medianG, medianB = median_absolute_deviation(im[:, :, 0].flatten(), axis=None), median_absolute_deviation(im[:, :, 1].flatten(), axis=None), median_absolute_deviation(im[:, :, 2].flatten(), axis=None)
-
+    medianR, medianG, medianB = st.median(im[:, :, 0].flatten()), st.median(im[:, :, 1].flatten()), st.median(im[:, :, 2].flatten())
     stdR = np.sqrt(np.sum(np.square(np.subtract(im[:, :, 0],meanR)))/(total_pix-1))
     stdG = np.sqrt(np.sum(np.square(np.subtract(im[:, :, 1],meanG)))/(total_pix-1))
     stdB = np.sqrt(np.sum(np.square(np.subtract(im[:, :, 2],meanB)))/(total_pix-1))
@@ -78,7 +76,6 @@ def statistic_extractor(image):
     kurtosisR, kurtosisG, kurtosisB = kurtosis(im[:, :, 0].flatten()), kurtosis(im[:, :, 1].flatten()), kurtosis(im[:, :, 2].flatten())
     return np.array([
         [meanR, meanG, meanB],
-        [modeR, modeG, modeB],
         [medianR, medianG, medianB],
         [stdR, stdG, stdB],
         [skewR, skewG, skewB],
@@ -89,24 +86,16 @@ def geometry_extractor(image):
     im = image
     gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     contours, hierarchy = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
-    # idx =0 
     for cnt in contours:
-        # idx += 1
         x,y,w,h = cv2.boundingRect(cnt)
         if (w>50) and (h>100):
-            # wid = int(max(h,w))
-            # im=im[y-max(0,int((w-h)/2)):y+wid-max(0,int((w-h)/2)), x-max(0,int((h-w)/2)):x+wid-max(0,int((h-w)/2))]
-            # im = im[y:y+h,x:x+w]
-            # break
             convexHull = cv2.convexHull(cnt)
             convex_area = cv2.contourArea(convexHull)
             # cv2.drawContours(image, [convexHull], -1, (255, 0, 0), 2)
-
             perimeter = cv2.arcLength(cnt,True)
             # approximatedShape = cv2.approxPolyDP(cnt, 0.002 * perimeter, True)
             # print(perimeter, approximatedShape)
             # cv2.drawContours(image, [approximatedShape], -1, (255, 255, 0), 2)
-
             (centerXCoordinate, centerYCoordinate), eq_radius = cv2.minEnclosingCircle(cnt)
             cv2.circle(image, (int(centerXCoordinate), int(centerYCoordinate)), int(eq_radius), (0,0,255), 2)
             cv2.circle(image, (int(centerXCoordinate), int(centerYCoordinate)), 10, (255,0,0), 5)
@@ -126,10 +115,37 @@ def geometry_extractor(image):
             solidity = real_area/convex_area
             bb_ratio = real_area/(w*h)
             eccentricity_distance = np.sqrt( (centerXCoordinate-centroidXCoordinate)**2 + (centerYCoordinate-centroidYCoordinate)**2 )
-            return real_area, perimeter, (alX, alY), orientation, ellipse_eccentricity, convex_area, eq_radius, solidity, bb_ratio, eccentricity_distance
+            return (y,y+h,x,x+w),real_area, perimeter, (alX, alY), orientation, ellipse_eccentricity, convex_area, eq_radius, solidity, bb_ratio, eccentricity_distance
 
 
 def draw_bb(im,data):
     res = np.copy(im)
     (x,y,w,h) = data
     return cv2.rectangle(res,(x,y),(x+w,y+h),(200,0,0),2)
+
+def CLAHE(grey_img, grey=False):
+    if grey==False:
+        grey_img = cv2.cvtColor(grey_img, cv2.COLOR_BGR2GRAY)
+    hist,bins = np.histogram(grey_img.flatten(),256,[0,256])
+    cdf = hist.cumsum()
+    cdf_m = np.ma.masked_equal(cdf,0)
+    cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+    cdf = np.ma.filled(cdf_m,0).astype('uint8')
+    return cdf[grey_img]
+
+def contour_extractor(image):
+    
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+def seperate_chanel(image, plot=False):
+    h,s,v = image[:,:,0], image[:,:,1], image[:,:,2]
+    if plot==True:
+        plt.subplot(1,3,1)
+        plt.imshow(h)
+        plt.subplot(1,3,2)
+        plt.imshow(s)
+        plt.subplot(1,3,3)
+        plt.imshow(v)
+        plt.show()
+    return h,s,v
